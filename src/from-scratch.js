4367dc42bb1ad95d70325140c396bf4dc797c5aa
EngelsGB <engelsgarciabello@gmail.com>
2024-06-10 22:15:34 -0400

    Oh yeah, it's all coming together

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,16 +1,35 @@
-const resolvedWrapper = () => {
+const resolvedWrapper = (num) => {
+  return Promise.resolve(num);
 };
 
-const rejectedWrapper = () => {
+const rejectedWrapper = (msg) => {
+  return Promise.reject(new Error(msg));
 };
 
-const handleResolvedPromise = () => {
+const handleResolvedPromise = (promise) => {
+  return promise
+    .then((val) => {
+      console.log(val);
+      return val;
+    });
 };
 
-const handleResolvedOrRejectedPromise = () => {
+const handleResolvedOrRejectedPromise = (promise) => {
+  return promise
+    .then((val) => {
+      console.log(val);
+      return val;
+    })
+    .catch((err) => {
+      console.error(`Your error message was: ${err.message}`);
+      return null;
+    })
 };
 
-const pauseForMs = () => {
+const pauseForMs = (timer) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, timer);
+  });
 };
 
 module.exports = {
